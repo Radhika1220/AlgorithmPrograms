a608815ca94a3fb6e82b69b437ff6fe97c996033
Radhika <radhika.shankar1220@gmail.com>
2021-07-12 20:38:47 +0530

    [Radhika] Binary search in a word

--- AlgorithmPrograms1/Program.cs
+++ AlgorithmPrograms1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace AlgorithmPrograms1
 {
@@ -7,24 +9,34 @@ namespace AlgorithmPrograms1
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Algorithm Programs!");
+            string flag = "Y";
+            while (flag == "Y" || flag == "y")
+            {
             Console.WriteLine("1.Permutation Using Recursion");
+            Console.WriteLine("3.Binary Search the word from wordlist");
             Console.WriteLine("Enter the option");
             int num = Convert.ToInt32(Console.ReadLine());
-            switch(num)
-            {
-                case 1:
-                    Console.WriteLine("Enter String to find Permutation ");
-                    string str = Console.ReadLine();
-                    int n = str.Length;
-                    Permutations.Permute(str, 0, n - 1);
-                    break;
-
-                default:
-                    Console.WriteLine("Enter the valid option!");
-                    break;
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine("Enter String to find Permutation ");
+                        string str = Console.ReadLine();
+                        int n = str.Length;
+                        Permutations.Permute(str, 0, n - 1);
+                        break;
+                    case 3:
+                        string txtpath = File.ReadAllText(@"C:\Users\Radhika\source\repos\AlgorithmPrograms1\ListOfWords.txt");
+                        List<string> words = new List<string>(txtpath.Split(" "));
+                        words.Sort();
+                        BinarySearch.BinarySearchWord(words);
+                        break;
+                    default:
+                        Console.WriteLine("Enter the valid option!");
+                        break;
+                }
+                Console.WriteLine("\nDo you want to continue");
+               flag= Console.ReadLine();
             }
-            Console.WriteLine("\nDo you want to continue");
-            Console.ReadLine();
         }
     }
 }
