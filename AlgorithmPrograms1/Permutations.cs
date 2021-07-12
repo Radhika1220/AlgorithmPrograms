6734431fb01d84069c023874bf7d558844c454aa
Radhika <radhika.shankar1220@gmail.com>
2021-07-12 19:53:52 +0530

    [Radhika] permutation using recursion

---
+++ AlgorithmPrograms1/Permutations.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmPrograms1
+{
+    class Permutations
+    {
+        public static void Permute (string str,int start, int end)
+        {
+            if(start==end)
+            {
+                Console.WriteLine(str);
+            }
+            else
+            {
+                for(int i=start;i<=end;i++)
+                {
+                    str = Swap(str, start, i);
+                    Permute(str, start + 1, end);
+                    str = Swap(str, start, i);
+                }
+            }
+        }
+
+        public static string Swap(string word,int start, int index)
+        {
+            char temp;
+            char[] charArray = word.ToCharArray();
+            temp = charArray[start];
+            charArray[start] = charArray[index];
+            charArray[index] = temp;
+
+            string s = new string(charArray);
+            return s;
+        }
+    }
+}
--- AlgorithmPrograms1/Program.cs
+++ AlgorithmPrograms1/Program.cs
@@ -7,6 +7,24 @@ namespace AlgorithmPrograms1
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Algorithm Programs!");
+            Console.WriteLine("1.Permutation Using Recursion");
+            Console.WriteLine("Enter the option");
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch(num)
+            {
+                case 1:
+                    Console.WriteLine("Enter String to find Permutation ");
+                    string str = Console.ReadLine();
+                    int n = str.Length;
+                    Permutations.Permute(str, 0, n - 1);
+                    break;
+
+                default:
+                    Console.WriteLine("Enter the valid option!");
+                    break;
+            }
+            Console.WriteLine("\nDo you want to continue");
+            Console.ReadLine();
         }
     }
 }
