d75c40ff329037e36cd5d5a41ad34d05bba5129e
Radhika <radhika.shankar1220@gmail.com>
2021-07-12 13:37:52 +0530

    [Radhika] Initial commit

--- AlgorithmPrograms1/Program.cs
+++ AlgorithmPrograms1/Program.cs
@@ -6,7 +6,7 @@ namespace AlgorithmPrograms1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Algorithm Programs!");
         }
     }
 }
